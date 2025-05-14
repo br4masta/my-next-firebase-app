33d06bb5fad369d642235449dbc69d8b282c678a
Brianbramasta <Brianaldybramasta@gmail.com>
2025-05-14 15:58:19 +0700

    update acces

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3000']
+const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3000', 'https://brianaldybramasta.my.id']
 
 export function middleware(request: NextRequest) {
   // Handle CORS for API routes
